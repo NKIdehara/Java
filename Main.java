0796f95f25621ae51ea144990718c23aa7a1081b
Nelson Idehara <113538333+NKIdehara@users.noreply.github.com>
2022-10-30 10:16:11 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,19 @@
+/***************************************************************************************
+ *
+ *  TESTE DE PERFORMANCE - TP1
+ *  Instituto Infnet
+ *  Engenharia de Software
+ *
+ *  Fundamentos do Desenvolvimento Java 
+ *
+ *  Aluno: Nelson Kenji Idehara
+ *  Professor: Elberth Moraes
+ *  Data: 31 Outubro 2022
+ *
+ ***************************************************************************************/
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
