ad5b5dd13ff3264a58cf52c94f9606aa4506fda4
Nelson Idehara <113538333+NKIdehara@users.noreply.github.com>
2022-10-30 10:31:56 +0000

    02

--- nelson_idehara_DR1_TP1.java
+++ nelson_idehara_DR1_TP1.java
@@ -1,19 +1,20 @@
 /***************************************************************************************
  *
- *  TESTE DE PERFORMANCE - TP1
- *  Instituto Infnet
- *  Engenharia de Software
+ * TESTE DE PERFORMANCE - TP1
+ * Instituto Infnet
+ * Engenharia de Software
  *
- *  Fundamentos do Desenvolvimento Java 
+ * Fundamentos do Desenvolvimento Java
  *
- *  Aluno: Nelson Kenji Idehara
- *  Professor: Elberth Moraes
- *  Data: 31 Outubro 2022
+ * Aluno: Nelson Kenji Idehara
+ * Professor: Elberth Moraes
+ * Data: 31 Outubro 2022
  *
  ***************************************************************************************/
 
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!!!");
+    System.out.println("Hello world!");
+    System.out.println("Hello world!");
   }
 }
